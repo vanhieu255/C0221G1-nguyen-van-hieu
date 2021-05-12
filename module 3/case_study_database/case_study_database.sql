7dae76b48fce32b879078041f5d8c493b4d44a57
Your Name <you@example.com>
2021-05-12 16:55:11 +0700

    up

--- module 3/case_study_database/case_study_database.sql
+++ module 3/case_study_database/case_study_database.sql
@@ -1,5 +1,5 @@
-create database casestudy1;
-create table `casestudy1`.`nhan_vien`(
+create database case_study;
+create table `case_study`.`nhan_vien`(
 id_nhan_vien int not null,
 ho_ten int ,
 id_vi_tri int ,
@@ -12,28 +12,28 @@ sdt  varchar(45),
 email  varchar(45),
 dia_chi  varchar(45),
 primary key (`id_nhan_vien`));
-drop table nhanvien;
 
-create table `casestudy1`.`vi_tri`(
+
+create table `case_study`.`vi_tri`(
 id_vi_tri int not null,
 ten_vi_tri varchar(45),
 primary key(`id_vi_tri`));
-drop table vitri;
 
 
-create table `casestudy1`.`trinh_do`(
+
+create table `case_study`.`trinh_do`(
 id_trinh_do int not null,
 trinhdo varchar(45),
 primary key(`id_trinh_do`));
-drop table trinhdo;
 
-create table `casestudy1`.`bo_phan`(
+
+create table `case_study`.`bo_phan`(
 id_bo_phan int not null,
 ten_bo_phan varchar(45),
 primary key(`id_bo_phan`));
-drop table bophan;
 
-create table `casestudy1`.`hop_dong`(
+
+create table `case_study`.`hop_dong`(
 id_hop_dong int not null,
 id_nhan_vien int ,
 id_khach_hang int,
@@ -43,25 +43,25 @@ ngay_ket_thuc date,
 tien_dat_coc int ,
 tong_tien int,
 primary key (`id_hop_dong`));
-drop table hopdong;
 
-create table `casestudy1`.`hop_dong_chi_tiet`(
+
+create table `case_study`.`hop_dong_chi_tiet`(
 id_hop_dong_chi_tiet int not null,
 id_hop_dong int,
 id_dich_vu_di_kem int,
 so_luong int ,
 primary key(`id_hop_dong_chi_tiet`));
-drop table hopdongchitiet;
 
-create table `casestudy1`.`dich_vu_di_kem`(
+
+create table `case_study`.`dich_vu_di_kem`(
 id_dich_vu_di_kem int not null,
 ten_dich_vu_di_kem varchar(45),
 gia int,
 don_vi int,
 trang_thai_kha_dung varchar(45),
 primary key (`id_dich_vu_di_kem`));
-drop table dichvudikem;
-create table `casestudy1`.`dich_vu`(
+
+create table `case_study`.`dich_vu`(
 id_dich_vu int not null,
 ten_dich_vu varchar(45),
 dien_tich int,
@@ -72,9 +72,9 @@ id_kieu_thue int,
 id_loai_dich_vu int,
 trang_thai varchar(45),
 primary key (`id_dich_vu`));
-drop table dich_vu;
 
-create table `casestudy1`.`khach_hang`(
+
+create table `case_study`.`khach_hang`(
 id_khach_hang int not null,
 id_loai_khach int,
 ho_ten varchar(45),
@@ -85,18 +85,18 @@ email varchar(45),
 dia_chi varchar(45),
 primary key (`id_khach_hang`));
 
-create table `casestudy1`.`loai_khach`(
+create table `case_study`.`loai_khach`(
 id_loai_khach int not null,
 ten_loai_khach varchar(45),
 primary key(`id_loai_khach`));
 
-create table `casestudy1`.`kieu_thue`(
+create table `case_study`.`kieu_thue`(
 id_kieu_thue int not null,
 ten_kieu_thue varchar(45),
 gia int,
 primary key (`id_kieu_thue`));
 
-create table `casestudy1`.`loai_dich_vu`(
+create table `case_study`.`loai_dich_vu`(
 id_loai_dich_vu int not null,
 ten_loai_dich_vu varchar(45),
 primary key (`id_loai_dich_vu`));
