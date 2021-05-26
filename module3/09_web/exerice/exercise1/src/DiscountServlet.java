1159e64d48dd3885485f03d1fc9c6de00d75812a
Your Name <you@example.com>
2021-05-26 17:39:43 +0700

    up bai 9

---
+++ module3/09_web/exerice/exercise1/src/DiscountServlet.java
@@ -0,0 +1,36 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "DiscountServlet" ,value = "/discount")
+public class DiscountServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String productDescription=request.getParameter("Product Description");
+        float listPrice=Float.parseFloat(request.getParameter("List Price"));
+        float discountPercent=Float.parseFloat(request.getParameter("Discount Percent"));
+
+        float discountAmount = (float) (listPrice * discountPercent* 0.01);
+        float discountPrice =listPrice -discountAmount;
+        PrintWriter writer = response.getWriter();
+        writer.println("<html>");
+        writer.println("<h1>productDescription: " + productDescription + "</h1>");
+        writer.println("<h1>List Price: " + listPrice+ "</h1>");
+        writer.println("<h1>Discount Percent: " + discountPercent + "</h1>");
+        writer.println("<h1>discountAmount: " + discountAmount+ "</h1>");
+        writer.println("<h1>discountPrice: " + discountPrice + "</h1>");
+        writer.println("</html>");
+
+
+
+
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
---
+++ module3/09_web/exerice/exercise1/web/index.jsp
@@ -0,0 +1,24 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Admin
+  Date: 5/26/2021
+  Time: 5:00 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>Product Discount Calculator</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+  </head>
+  <body>
+  <h2>Product Discount Calculator</h2>
+  <form action="/discount" method="post">
+    <input type="text" name="Product Description" placeholder="Product Description: "/>
+    <input type="text" name="List Price" placeholder="List Price: "/>
+    <input type="text" name="Discount Percent" placeholder="Discount Percent: "/>
+    <input type = "submit" id = "submit" value = "Calculate Discount"/>
+  </form>
+
+  </body>
+</html>
